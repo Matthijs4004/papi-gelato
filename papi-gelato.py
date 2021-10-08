33ccbaf562c4c4e3ac22b117897287727d644d3c
Matthijs Raatgever <matthijs4004@gmail.com>
2021-10-08 12:43:48 +0200

    paar kleine aanpassingen

--- papi-gelato.py
+++ papi-gelato.py
@@ -1,68 +1,175 @@
+# papi-gelato 2
+import math
 
-x = ("                          --------+")
 
-print("\nWelkom bij Papi Gelato.\n")
+print("Welkom bij Papi Gelato.")
+sorry = "Sorry dat is geen optie die we aanbieden..."
+hoorntje = "Hoorntjes    1 x € 1.25    = € 1.25  "
+bakje = "Bakjes       1 x € 0.75    = € 0.75  "
+x = ("                          -------- +")
 
 def bon():
+    sprPrijs = hoeveel*0.30
+    bolPrijs = hoeveel*0.95
     print("\n----------[Papi Gelato]----------\n")
-    print("Bolletjes   ",hvl," x €1,10   = €",1.10*hvl)
-    if houder == "hoorntje":
-        print("Hoorntjes    1 x €1,25    = € 1,25  ")
+    print("Bolletjes   ",hoeveel,"x € 1.10    = €",bolPrijs)
+    if topping == "Geen" and houder == "hoorntje":
+        print(hoorntje)
         print(x)
-        print("Totaal                    = €",(hvl*1.10)+1,25)
-    if houder == "bakje":
-        print("Bakjes       1 x €0,75    = € 0,75  ")
+        print("Totaal                    = €",bolPrijs+1.25)
+    if topping == "Geen" and houder == "bakje":
+        print(bakje)
         print(x)
-        print("Totaal                    = €",(hvl*1.10)+0.75)
+        print("Totaal                    = €",bolPrijs+0.75)
+    if topping == "Slagroom" and houder == "bakje":
+        print(bakje)
+        print("Topping      1 x € 0.50    = € 0.50  ")
+        print(x)
+        print("Totaal                     = €",bolPrijs+0.50+0.75)
+    elif topping == "Slagroom" and houder == "hoorntje":
+        print(hoorntje)
+        print("Topping      1 x € 0.50    = € 0.50  ")
+        print(x)
+        print("Totaal                     = €",bolPrijs+0.50+1.25)
+    if topping == "Sprinkels" and houder == "hoorntje":
+        print(hoorntje)
+        print("Topping      1 x €",sprPrijs,"    = €",sprPrijs)
+        print(x)
+        print("Totaal                     = €",bolPrijs+sprPrijs+1.25)
+    elif topping == "Sprinkels" and houder == "bakje":
+        print(bakje)
+        print("Topping      1 x €",sprPrijs,"    = €",sprPrijs)
+        print(x)
+        print("Totaal                     = €",bolPrijs+sprPrijs+0.75)
+    if topping == "Caramel" and houder == "hoorntje":
+        print(hoorntje)
+        print("Topping      1 x € 0.60  = € 0.60")
+        print(x)
+        print("Totaal                    = €",(bolPrijs+0.60+1.25))
+    elif topping == "Caramel" and houder == "bakje":
+        print(bakje)
+        print("Topping      1 x € 0.90    = € 0.90")
+        print(x)
+        print("Totaal                     = €",(bolPrijs+0.90+0.75))
 
+def round_half_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n*multiplier + 0.5) / multiplier
 
-
-
-def stap_1():
-    global hvl
-    hvl = int(input("Hoeveel bolletjes wilt u? "))
-    if hvl in range(1, 4):
-        smaak_keuze()
-    elif hvl in range(4, 8):
-        print("Dan krijgt u van mij een bakje met " + str(hvl) + " bolletjes\n")
-        smaak_keuze()
-    elif hvl >= 8:
-        print("Sorry, zulke grote bakken hebben we niet")   
-    else:
-        print("Sorry dat snap ik niet...")
-
-def smaak_keuze():
-    aantal = hvl
-    for x in range(hvl):
-        smaak = input("Welke smaak wilt u voor bolletje nummer " + str(aantal) + "? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ")
-    if smaak.lower().strip() == "A" or "C" or "M" or "V":
-        stap_2()
-    else:
-        print("Sorry dat snap ik niet...")
-        return smaak_keuze()
+def zakelijkbon():
+    literPrijs = liter*9.80
+    quotient = literPrijs / 100
+    procent = quotient*6
     
+    print("\n----------[Papi Gelato]----------\n")
+    print("Liter     ",liter," x € 9.80    = €",round_half_up(literPrijs, 2))
+    print(x)
+    print("Totaal                    = €",round_half_up(literPrijs))
+    print("BTW (6%)                  = €",round_half_up(procent))
 
-def stap_2():
+def start():
+    print("\nBent u 1) particulier of 2) zakelijk?")
+    markt = int(input("> "))
+    if markt == 1:
+        stap1()
+    elif markt == 2:
+        zakelijk1()
+    else:
+        print(sorry)
+        return start()
+
+def zakelijk1():
+    global liter
+    print("\nHoeveel liter ijs wilt u?")
+    liter = int(input("> "))
+    if liter > 1:
+        zakelijk2()
+    else:
+        print(sorry)
+        return zakelijk1
+
+def zakelijk2():
+    for x in range(liter, 0, -1):
+        print("\nWelke smaak wilt u voor liter nummer",x,"? A) Aardbei, C) Chocolade of V) Vanille")
+        smaakL = input("> ")
+    if smaakL == "A" or smaakL == "C" or smaakL == "V":
+        zakelijkbon()
+    else:
+        print(sorry)
+        return zakelijk2()
+
+def stap1():
+    global hoeveel
+    print("\nHoeveel bolletjes wilt u?")
+    hoeveel = int(input("> "))
+    if hoeveel in range(1,4):
+        smaakKeuze()
+    elif hoeveel in range(4,8):
+        print("Dan krijgt u van mij een bakje met ",hoeveel," bolletjes")
+        smaakKeuze()
+    elif hoeveel > 8:
+        print("Sorry, zulke grote bakken hebben we niet")
+        return stap1()
+    else:
+        print(sorry)
+        return stap1()
+
+
+def smaakKeuze():
+    for x in range(hoeveel, 0, -1):
+        print("\nWelke smaak wilt u voor bolletje nummer",x,"? A) Aardbei, C) Chocolade, M) Munt of V) Vanille")
+        smaak = input("> ")
+    if smaak == "A" or smaak == "C" or smaak == "M" or smaak == "V":
+        stap2()
+    else:
+        print(sorry)
+        return smaakKeuze()
+
+
+def stap2():
     global houder
-    houder = input("Wilt u deze " + str(hvl) +  " bolletje(s) in A) een hoorntje of B) een bakje? ")
+    print("\nWilt u deze",hoeveel,"bolletje(s) in A) een hoorntje of B) een bakje?")
+    houder = input("> ")
     if houder == "A":
         houder = "hoorntje"
-        stap_3("hoorntje")
+        toppings()
     elif houder == "B":
         houder = "bakje"
-        stap_3("bakje")
+        toppings()
     else:
-        print("Sorry dat snap ik niet...")
-        return stap_2()
+        print(sorry)
+        return stap2()
 
-def stap_3(houder):
-    nogmaals = input("Hier is uw " + houder + " met " + str(hvl) + " bolletje(s). Wilt u nog meer bestellen? (ja/nee) ")
-    if nogmaals.lower().strip() == "ja":
-        stap_1()
-    elif nogmaals.lower().strip() == "nee":
+
+def toppings():
+    global topping
+    print("Wat voor topping wilt u: A) Geen, B) Slagroom, C) Sprinkels of D) Caramel Saus?")
+    topping = input("> ")
+    if topping == "A":
+        topping == "Geen"
+        stap3()
+    elif topping == "B":
+        topping = "Slagroom"
+        stap3()
+    elif topping == "C":
+        topping = "Sprinkels"
+        stap3()
+    elif topping == "D":
+        topping = "Caramel"
+        stap3()
+    else:
+        print(sorry)
+
+
+def stap3():
+    print("\nHier is uw",houder,"met",hoeveel,"bolletje(s). Wilt u nog meer bestellen? Y/N")
+    nogmaals = input("> ")
+    if nogmaals == "Y":
+        stap1()
+    elif nogmaals == "N":
         bon()
-        print("\nBedankt en tot ziens!")
+        print("Bedankt en tot ziens!")
     else:
-        print("Sorry dat snap ik niet...")
+        print(sorry)
 
-stap_1()
+start()
